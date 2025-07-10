e94d23a70d41c1913f7ab5a211de699cf4907ead
MadDogOwner <xiaoran@xrgzs.top>
2025-07-10 16:03:51 +0800

    refactor(monaco): package monaco editor

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,17 @@ export default defineConfig({
   build: {
     // target: "es2015", //next
     // polyfillDynamicImport: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          jsonWorker: ["monaco-editor/esm/vs/language/json/json.worker"],
+          cssWorker: ["monaco-editor/esm/vs/language/css/css.worker"],
+          htmlWorker: ["monaco-editor/esm/vs/language/html/html.worker"],
+          tsWorker: ["monaco-editor/esm/vs/language/typescript/ts.worker"],
+          editorWorker: ["monaco-editor/esm/vs/editor/editor.worker"],
+        },
+      },
+    },
   },
   // experimental: {
   //   renderBuiltUrl: (filename, { type, hostId, hostType }) => {
