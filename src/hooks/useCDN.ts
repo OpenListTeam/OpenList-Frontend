97eadde986d31c69f43fc0a17afa79e518427e76
Suyunmeng <69945917+Suyunmeng@users.noreply.github.com>
2025-08-06 21:46:04 +0800

    fix: use static path for lite build in CDN hook

--- src/hooks/useCDN.ts
+++ src/hooks/useCDN.ts
@@ -1,7 +1,10 @@
 import { joinBase } from "~/utils"
 
 export const useCDN = () => {
-  const static_path = `${window.__dynamic_base__ || ""}/static`
+  const static_path =
+    import.meta.env.VITE_LITE === "true"
+      ? "/static"
+      : `${window.__dynamic_base__ || ""}/static`
 
   const npm = (name: string, version: string, path: string) => {
     // Available: https://github.com/cnpm/unpkg-white-list
