c606d6e2b3c626bb0933284b7363a5e50cbda45b
MadDogOwner <xiaoran@xrgzs.top>
2026-04-03 16:49:04 +0800

    feat(File): add `preview` search parameter
    Signed-off-by: MadDogOwner <xiaoran@xrgzs.top>

--- src/pages/home/file/File.tsx
+++ src/pages/home/file/File.tsx
@@ -1,26 +1,35 @@
 import { HStack, VStack } from "@hope-ui/solid"
-import { createMemo, createSignal, Show, Suspense } from "solid-js"
+import { createMemo, Show, Suspense } from "solid-js"
 import { Dynamic } from "solid-js/web"
 import { FullLoading, SelectWrapper } from "~/components"
 import { objStore } from "~/store"
+import { useRouter } from "~/hooks"
 import { Download } from "../previews/download"
 import { OpenWith } from "./open-with"
 import { getPreviews } from "../previews"
 
 const File = () => {
+  const { searchParams, setSearchParams } = useRouter()
   const previews = createMemo(() => {
     return getPreviews({ ...objStore.obj, provider: objStore.provider })
   })
-  const [cur, setCur] = createSignal(previews()[0])
+  const selectedPreviewKey = createMemo(() => searchParams["preview"] || "")
+  const cur = createMemo(() => {
+    const list = previews()
+    if (list.length === 0) return undefined
+    const selected = selectedPreviewKey()
+    return list.find((p) => p.key === selected) || list[0]
+  })
+
   return (
     <Show when={previews().length > 1} fallback={<Download openWith />}>
       <VStack w="$full" spacing="$2">
         <HStack w="$full" spacing="$2">
           <SelectWrapper
             alwaysShowBorder
-            value={cur().key}
+            value={cur()?.key || ""}
             onChange={(key) => {
-              setCur(previews().find((p) => p.key === key)!)
+              setSearchParams({ preview: key }, { replace: true })
             }}
             options={previews().map((item) => ({
               value: item.key,
@@ -30,7 +39,7 @@ const File = () => {
           <OpenWith />
         </HStack>
         <Suspense fallback={<FullLoading />}>
-          <Dynamic component={cur().component} />
+          <Dynamic component={cur()?.component} />
         </Suspense>
       </VStack>
     </Show>
